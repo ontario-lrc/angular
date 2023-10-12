561ba1912980d4a4bec070e7918aca5e535653a7
Kasperowicz, Dawid <dawid.kasperowicz@ontario.ca>
2023-10-12 18:24:07 -0400

    Added heading level customization to page-alert

--- projects/page-alert/src/lib/page-alert.component.ts
+++ projects/page-alert/src/lib/page-alert.component.ts
@@ -11,7 +11,8 @@ import {ChangeDetectionStrategy, Component, Input, ViewEncapsulation} from "@ang
 
 export class OdsPageAlertComponent
 {
-	@Input() type: OdsPageAlertType = OdsPageAlertType.INFORMATIONAL;
+	@Input() type: OdsPageAlertType = OdsPageAlertType.INFORMATION;
+	@Input() headingLevel!: OdsPageAlertHeadingLevel;
 	@Input() heading!: string;
 	@Input() message!: string;
 	@Input() iconAltText!: string;
@@ -20,12 +21,23 @@ export class OdsPageAlertComponent
 
 const OdsPageAlertType =
 {
-	INFORMATIONAL: "informational" as "informational",
+	INFORMATION: "information" as "information",
 	WARNING: "warning" as "warning",
 	SUCCESS: "success" as "success",
 	ERROR: "error" as "error"
 };
 
+const OdsPageAlertHeadingLevel =
+{
+	H1: "h1" as "h1",
+	H2: "h2" as "h2",
+	H3: "h3" as "h3",
+	H4: "h4" as "h4",
+	H5: "h5" as "h5",
+	H6: "h6" as "h6"
+};
+
 type OdsPageAlertType = (typeof OdsPageAlertType)[keyof typeof OdsPageAlertType];
+type OdsPageAlertHeadingLevel = (typeof OdsPageAlertHeadingLevel)[keyof typeof OdsPageAlertHeadingLevel]
 
 export{OdsPageAlertType};
